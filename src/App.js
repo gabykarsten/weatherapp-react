226656b755bc7453e5fa6a52f44919bb7b3145e2
gabykarsten <karstengaby@gmail.com>
2024-04-26 17:12:55 -0700

    update the header on App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello world
+        Weather App
         <Weather />
       </header>
     </div>
